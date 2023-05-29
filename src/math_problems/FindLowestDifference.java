e943a2f5f4b3026ec78847907fcb200caf6e329a
Dalal-QA <sadouni,dalal@gmail.com>
2023-05-29 19:34:35 -0400

    Solved the third math problem'finflowestDifference'

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -1,5 +1,8 @@
 package math_problems;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class FindLowestDifference {
 
     /** INSTRUCTIONS
@@ -7,11 +10,40 @@ public class FindLowestDifference {
      * HINT: The lowest number that isn't shared between these arrays is 1
      */
 
+
+    public static int returnLowestNumber(int [] array1, int array2 []){
+        int lowestNumber = 1; //this is the starting point to search for the lowest not shared  number
+       while(true){
+           boolean isItInArray1 = false;
+           boolean isInArray2 =false;
+
+           for(int x: array1){
+               if (x ==lowestNumber){
+                   isItInArray1 = true;
+                   break;
+               }
+           }
+           for (int y: array2){
+               if(y == lowestNumber){
+                   isInArray2 =true;
+                   break;
+               }
+
+           }
+           if(isItInArray1!= isInArray2){
+               return lowestNumber;
+           }
+           lowestNumber++;
+       }
+    }
     public static void main(String[] args) {
 
 
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
+
+     int lowestNotSharedNumber =returnLowestNumber(array1,array2);
+        System.out.println("the lowest number that is not shared bettween the 2 arrays is "+ lowestNotSharedNumber );
 
     }
 
