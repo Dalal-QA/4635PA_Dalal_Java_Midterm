82f81f911c59a84e9c1f60ea5d43ce9aceb8110e
Dalal-QA <sadouni,dalal@gmail.com>
2023-05-29 17:31:23 -0400

    First math problem/factorial/ resolved

--- src/math_problems/Factorial.java
+++ src/math_problems/Factorial.java
@@ -9,10 +9,55 @@ public class Factorial {
      *
      * HINT: Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120
      */
+    static int factIt=1;
+    static int result;
+    static int x;
+    static int y;
 
-    public static void main(String[] args) {
+    //This method is returning me the factorial of any given number using iteration
+    public static int getFactorialByIteration(int x){
 
-
+        for(int i = 1; i<=x; i++){
+           factIt=factIt*i;
+        }
+      return factIt;
     }
 
+    //This method is returning me the factorial of any given number using Recursion
+
+    public static int getFactorialByRecursion(int y){
+        if(y==0 || y==1){
+            return 1;
+        }else{
+            result = getFactorialByRecursion(y-1) *y;
+            return result;
+
+        }
+    }
+    public static void main(String[] args) {
+
+        Factorial.getFactorialByIteration(7);
+        System.out.println("The factorial of "+ x + " is: "+ factIt);
+
+        Factorial.getFactorialByRecursion(8);
+        System.out.println("The factorial of "+ y + " is:"+ result );
+
+
+        }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
