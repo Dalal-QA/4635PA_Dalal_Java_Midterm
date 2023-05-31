d6fd347b283bfc6c3fa654932619da1e64b45ed9
Dalal-QA <sadouni,dalal@gmail.com>
2023-05-30 22:28:31 -0400

    Duplicate word within string problems completed

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string_problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateWord {
 
     /** INSTRUCTIONS
@@ -9,8 +12,36 @@ public class DuplicateWord {
 
     public static void main(String[] args) {
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+          //split() method used to split the string into words
+        String[] words = st.split("\\s+");
 
-        // Implement here
+        // Create a HashMap to store word frequency
+        Map<String, Integer> wordFrequency = new HashMap<>();
+
+        // Calculate total word length
+        int totalLength = 0;
+
+        // Process each word
+        for (String word : words) {
+
+            wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+
+
+            totalLength += word.length();
+        }
+
+
+        double averageLength = (double) totalLength / words.length;
+
+        System.out.println("Duplicate words and their occurrences:");
+        for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " - " + entry.getValue() + " occurrences");
+            }
+        }
+
+        // Print average word length
+        System.out.println("Average word length: " + averageLength);
 
     }
 
